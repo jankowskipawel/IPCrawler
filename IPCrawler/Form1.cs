4a67eead34261d04b713b8900166d697a0fef8b7
jankowskipawel <39927846+jankowskipawel@users.noreply.github.com>
2021-07-12 12:41:55 +0200

    Add ip info getting from api

--- IPCrawler/Form1.cs
+++ IPCrawler/Form1.cs
@@ -5,7 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -51,9 +53,51 @@ namespace IPCrawler
             {
                 ipArray[i] = lines[i].Split('-').First().Trim();
             }
+            int j = 0;
+            //split array every 100 elements
+            var query = from s in ipArray
+                        let num = j++
+                group s by num / 100 into g
+                select g.ToArray();
+            var ipArrays = query.ToArray();
             //get ip data
+            List<string> ipData = new List<string>(lines.Length);
+            foreach (var ipArr in ipArrays)
+            {
+                string apiString = CreateApiString(ipArr);
+                ipData = GetIPData(apiString, ipData);
+            }
+
             //generate csv
 
+        }
+
+        public string CreateApiString(string[] ipArray)
+        {
+            string apiString = "[";
+            for (int i = 0; i < ipArray.Length; i++)
+            {
+                apiString += $"\"{ipArray[i]}\",";
+            }
+            apiString = apiString.TrimEnd(',');
+            apiString += "]";
+            return apiString;
+        }
+
+        public List<string> GetIPData(string apiString, List<string> ipDataList)
+        {
+            string[] ipData = new string[100];
+            using (WebClient wc = new WebClient())
+            {
+                var result = wc.UploadString("http://ip-api.com/batch?fields=58105", "POST", apiString);
+                result = result.TrimStart('[').TrimEnd(']');
+                ipData = result.Split('}');
+            }
+            foreach (var ipSingleData in ipData)
+            {
+                ipDataList.Add(ipSingleData.TrimStart(',').Trim() + "}");
+            }
+            return ipDataList.SkipLast(1).ToList();
         }
 
         public static bool IsPathValid(string filePath)
@@ -88,4 +132,39 @@ namespace IPCrawler
             }
         }
     }
+
+
+
+    public class Rootobject
+    {
+        public string ip { get; set; }
+        public string version { get; set; }
+        public string city { get; set; }
+        public string region { get; set; }
+        public string region_code { get; set; }
+        public string country { get; set; }
+        public string country_name { get; set; }
+        public string country_code { get; set; }
+        public string country_code_iso3 { get; set; }
+        public string country_capital { get; set; }
+        public string country_tld { get; set; }
+        public string continent_code { get; set; }
+        public bool in_eu { get; set; }
+        public string postal { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string timezone { get; set; }
+        public string utc_offset { get; set; }
+        public string country_calling_code { get; set; }
+        public string currency { get; set; }
+        public string currency_name { get; set; }
+        public string languages { get; set; }
+        public float country_area { get; set; }
+        public float country_population { get; set; }
+        public string message { get; set; }
+        public string asn { get; set; }
+        public string org { get; set; }
+    }
+
+
 }
