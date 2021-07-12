8906d375d8ac6dd2b512205ff0f3cb849d414997
jankowskipawel <39927846+jankowskipawel@users.noreply.github.com>
2021-07-12 10:32:15 +0200

    Add path validation

--- IPCrawler/Form1.Designer.cs
+++ IPCrawler/Form1.Designer.cs
@@ -29,23 +29,23 @@ namespace IPCrawler
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.inputFilePathTextBox = new System.Windows.Forms.TextBox();
             this.inputFilePickerButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.outputDirecrotyPickerButton = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.outputFolderPathTextBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.inputFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.outputBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
             this.GenerateCSVButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // inputFilePathTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 35);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(360, 23);
-            this.textBox1.TabIndex = 0;
+            this.inputFilePathTextBox.Location = new System.Drawing.Point(12, 35);
+            this.inputFilePathTextBox.Name = "inputFilePathTextBox";
+            this.inputFilePathTextBox.Size = new System.Drawing.Size(360, 23);
+            this.inputFilePathTextBox.TabIndex = 0;
             // 
             // inputFilePickerButton
             // 
@@ -55,6 +55,7 @@ namespace IPCrawler
             this.inputFilePickerButton.TabIndex = 1;
             this.inputFilePickerButton.Text = "...";
             this.inputFilePickerButton.UseVisualStyleBackColor = true;
+            this.inputFilePickerButton.Click += new System.EventHandler(this.inputFilePickerButton_Click);
             // 
             // label1
             // 
@@ -73,26 +74,27 @@ namespace IPCrawler
             this.outputDirecrotyPickerButton.TabIndex = 3;
             this.outputDirecrotyPickerButton.Text = "...";
             this.outputDirecrotyPickerButton.UseVisualStyleBackColor = true;
+            this.outputDirecrotyPickerButton.Click += new System.EventHandler(this.outputDirecrotyPickerButton_Click);
             // 
-            // textBox2
+            // outputFolderPathTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(13, 95);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(359, 23);
-            this.textBox2.TabIndex = 4;
+            this.outputFolderPathTextBox.Location = new System.Drawing.Point(13, 95);
+            this.outputFolderPathTextBox.Name = "outputFolderPathTextBox";
+            this.outputFolderPathTextBox.Size = new System.Drawing.Size(359, 23);
+            this.outputFolderPathTextBox.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 74);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(94, 15);
+            this.label2.Size = new System.Drawing.Size(109, 15);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Output file path:";
+            this.label2.Text = "Output folder path:";
             // 
-            // openFileDialog1
+            // inputFileDialog
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            this.inputFileDialog.FileName = "openFileDialog1";
             // 
             // GenerateCSVButton
             // 
@@ -102,6 +104,7 @@ namespace IPCrawler
             this.GenerateCSVButton.TabIndex = 6;
             this.GenerateCSVButton.Text = "Generate CSV";
             this.GenerateCSVButton.UseVisualStyleBackColor = true;
+            this.GenerateCSVButton.Click += new System.EventHandler(this.GenerateCSVButton_Click);
             // 
             // Form1
             // 
@@ -110,11 +113,11 @@ namespace IPCrawler
             this.ClientSize = new System.Drawing.Size(422, 185);
             this.Controls.Add(this.GenerateCSVButton);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.outputFolderPathTextBox);
             this.Controls.Add(this.outputDirecrotyPickerButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.inputFilePickerButton);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.inputFilePathTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -124,14 +127,14 @@ namespace IPCrawler
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox inputFilePathTextBox;
         private System.Windows.Forms.Button inputFilePickerButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button outputDirecrotyPickerButton;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox outputFolderPathTextBox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog inputFileDialog;
+        private System.Windows.Forms.FolderBrowserDialog outputBrowserDialog;
         private System.Windows.Forms.Button GenerateCSVButton;
     }
 }
--- IPCrawler/Form1.cs
+++ IPCrawler/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +18,65 @@ namespace IPCrawler
             InitializeComponent();
         }
 
+        private void inputFilePickerButton_Click(object sender, EventArgs e)
+        {
+            inputFileDialog.ShowDialog();
+            inputFilePathTextBox.Text = inputFileDialog.FileName;
+            outputFolderPathTextBox.Text = Path.GetDirectoryName(inputFileDialog.FileName);
+        }
+
+        private void outputDirecrotyPickerButton_Click(object sender, EventArgs e)
+        {
+            outputBrowserDialog.ShowDialog();
+            outputFolderPathTextBox.Text = outputBrowserDialog.SelectedPath;
+        }
+
+        private void GenerateCSVButton_Click(object sender, EventArgs e)
+        {
+            //check if paths are valid
+            try
+            {
+                CheckFilePath(inputFilePathTextBox.Text);
+                CheckDirectoryPath(outputFolderPathTextBox.Text);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                return;
+            }
+
+        }
+
+        public static bool IsPathValid(string filePath)
+        {
+            HashSet<char> invalidCharacters = new HashSet<char>(Path.GetInvalidPathChars());
+            return !string.IsNullOrEmpty(filePath) && !filePath.Any(pc => invalidCharacters.Contains(pc));
+        }
+
+        public static void CheckFilePath(string path)
+        {
+            if (path.Length == 0)
+            {
+                throw new ArgumentException("File path cannot be empty");
+            }
+
+            if (!File.Exists(path) || !IsPathValid(path))
+            {
+                throw new ArgumentException($"{path} is not a valid directory");
+            }
+        }
+
+        public static void CheckDirectoryPath(string path)
+        {
+            if (path.Length == 0)
+            {
+                throw new ArgumentException("Directory path cannot be empty");
+            }
+
+            if (!Directory.Exists(path) || !IsPathValid(path))
+            {
+                throw new ArgumentException($"{path} is not a valid directory");
+            }
+        }
     }
 }
